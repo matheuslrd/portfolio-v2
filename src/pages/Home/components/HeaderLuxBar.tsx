145ca07c21b4094866e31386c9fc4e331be05982
Matheus Laurindo <matheus104521@gmail.com>
2024-01-16 19:38:42 -0300

    chore: get old Homepage and config header

---
+++ src/pages/Home/components/HeaderLuxBar.tsx
@@ -0,0 +1,74 @@
+import { GlobalContext } from '@context/MyProvider';
+import { IGlobalContext } from '@context/interfaces';
+import { useContext } from 'react';
+
+// import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
+
+import Switch from 'react-switch';
+
+export default function HeaderLuxBar() {
+  const { theme, setTheme } = useContext<IGlobalContext>(GlobalContext);
+
+  function changeTheme() {
+    setTheme((prevTheme) => !prevTheme);
+    localStorage.setItem('theme', JSON.stringify(!theme));
+  }
+
+  return (
+    <header id="luxbar" className="luxbar-fixed">
+      <input type="checkbox" className="luxbar-checkbox" id="luxbar-checkbox" />
+      <nav className="luxbar-menu luxbar-menu-right luxbar-menu-dark">
+        <ul className="luxbar-navigation">
+          <li className="luxbar-header">
+            <a href="#" className="luxbar-brand Title-Name">
+              Matheus Laurindo
+            </a>
+            <label
+              className="luxbar-hamburger luxbar-hamburger-spin"
+              id="luxbar-hamburger"
+              htmlFor="luxbar-checkbox"
+            >
+              {' '}
+              <span className="hamburguer" />{' '}
+            </label>
+          </li>
+          <li className="luxbar-item">
+            <Switch
+              activeBoxShadow="none"
+              checked={theme}
+              uncheckedIcon={
+                // <BsFillSunFill color="#fff" size="0.9em" className="Light-Theme-Icon" />
+                <>a</>
+              }
+              checkedIcon={
+                // <BsFillMoonFill color="#fff" size="0.8em" className="Light-Theme-Icon" />
+                <>a</>
+              }
+              className="Switch-Theme"
+              handleDiameter={17}
+              height={17}
+              onChange={changeTheme}
+              onColor="#888"
+              width={45}
+            />
+          </li>
+          <li className="luxbar-item">
+            <a href="#About-Me">Sobre Mim</a>
+          </li>
+
+          <li className="luxbar-item">
+            <a href="#Projects">Projetos</a>
+          </li>
+
+          <li className="luxbar-item">
+            <a href="#Services">Serviços</a>
+          </li>
+
+          <li className="luxbar-item">
+            <a href="#Skills">Habilidades</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
-
-// import HeaderLuxBar from '../../Components/HeaderLuxBar';
-// import ContactMe from './components/ContactMe';
-// import ArrowDown from './components/ArrowDown';
-
-// import devIllustration from '../../images/illustrations/dev-illustration.svg';
+import HeaderLuxBar from './components/HeaderLuxBar';
+import devIllustration from '@assets/dev-illustration.svg';
 
 import StyledHomePage from './styles/homePage';
+import { WhatsappLogo, GithubLogo } from '@phosphor-icons/react';
 
 function HomePage() {
   return (
     <StyledHomePage>
-      {/* <section id="Home-Page">
+      <section id="Home-Page">
         <HeaderLuxBar />
-
         <main className="Main-Content">
           <section className="Contacte-Me-Container" data-aos="zoom-in">
             <div className="summary-container">
-              <ContactMe />
+              <div className="summary-about-me">
+                Olá, eu sou
+                <br />
+                <h2>Matheus Laurindo</h2>
+                <span className="Student-Web">Estudante de Desenvolvimento Web.</span>
+              </div>
+
+              <div className="buttons-contact">
+                <a
+                  className="btn-contact"
+                  href="https://wa.me/+5551984108411?text=Ol%C3%A1,%20Matheus"
+                  target="_blank"
+                >
+                  <WhatsappLogo size={28} />
+                  whatsapp
+                </a>
+
+                <a
+                  className="btn-contact btn-github"
+                  href="https://github.com/matheuslrd"
+                  target="_blank"
+                >
+                  <GithubLogo size={28} />
+                  GitHub
+                </a>
+              </div>
             </div>
           </section>
-          <section
-            className="illustration-dev-container"
-            data-aos="zoom-in"
-          >
+
+          <section className="illustration-dev-container" data-aos="zoom-in">
             <img
               className="illustration-dev"
               src={devIllustration}
@@ -31,17 +49,14 @@ function HomePage() {
             />
           </section>
         </main>
-
         <a href="#About-Me">
-          <div
-            className="arrow"
-            id="About-Me"
-          >
-            <ArrowDown />
+          <div className="arrow" id="About-Me">
+            <span />
+            <span />
+            <span />
           </div>
         </a>
-      </section> */}
-      Homepage
+      </section>
     </StyledHomePage>
   );
 }
