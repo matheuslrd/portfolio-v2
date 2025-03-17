09371a52fe5db33c27b12ff47e73fd90afd42ea0
Matheus Laurindo <matheus104521@gmail.com>
2025-03-17 17:46:40 -0300

    fix: join globalstyles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,58 +1,52 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 16px;
+  }
+
   * {
     border: 0;
     box-sizing: border-box;
     list-style: none;
     margin: 0;
     padding: 0;
-  }
-
-  html {
-    font-size: 16px;
+    scroll-behavior: smooth;
+    text-decoration: none;
   }
 
   html, body {
     height: auto;
     min-height: 100%;
-    background: #fff;
+    min-width: 300px;
+    overflow-x: hidden;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.color.primary};
+    color: ${({ theme }) => theme.color.text};
+    transition: all 0.3s ease;
   }
 
   body, input, textarea {
-    font-family: Montserrat, sans-serif;
+    font-family: 'Inter', 'Roboto', Montserrat, sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, p, span, a {
+    font-size: inherit;
   }
 
   *, a, button, div {
-
     &:focus-visible {
       outline: 0.187rem solid #cdd5f2;
     }
   }
 
   @media only screen and (max-width: 1440px) {
-
     html {
       font-size: 14px;
     }
   }
-
-  /* * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    scroll-behavior: smooth;
-    text-decoration: none;
-    outline: 0;
-  }
-  
-  body {
-    color: ${(props) => props.theme.color.text};
-    font-family: "Roboto", sans-serif;
-    font-size: 16px;
-    min-width: 300px;
-    overflow-x: hidden;
-  } */
 `;
 
 export default GlobalStyle;
