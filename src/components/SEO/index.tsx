5b91acc8718811ec2394086b5b27377971f4177a
Matheus Laurindo <matheus104521@gmail.com>
2025-03-18 13:31:56 -0300

    remove duplicates metatags

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -20,7 +20,6 @@ export function SEO({
   title = defaultTitle,
   description = defaultDescription,
   keywords = defaultKeywords,
-  ogImage = defaultOgImage,
   ogUrl = defaultUrl,
   twitterCreator = '@matheuslrd_',
   canonicalUrl = defaultUrl,
@@ -30,25 +29,16 @@ export function SEO({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <meta name="author" content="Matheus Laurindo" />
-      
       <meta name="robots" content="index, follow" />
       <link rel="canonical" href={canonicalUrl} />
       
-      <meta property="og:type" content="website" />
       <meta property="og:url" content={ogUrl} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={ogImage} />
       
-      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterCreator} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={ogImage} />
-      
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="theme-color" content="#5d2de2" />
       
       <script type="application/ld+json">
         {JSON.stringify({
